c6f63f028c1cee42d2a4ed96c99b20f91083a05d
Brendan Merritt <brendanmerritt1@gmail.com>
2023-09-13 01:24:04 -0400

    Vite code splitting deps

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import { dependencies } from "./package.json";
+
+function renderChunks(deps) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "react-dom"],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 });
